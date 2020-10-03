a48bb5a8ae1ebaac74b0f5ffdfe08c03c70ec0cd
tomer <tomerkubovsky@gmail.com>
2020-10-03 15:09:55 +1000

    SECUNDcOMMIT
    SecondCOmmit

--- Assets/Project/Scripts/Player.cs
+++ Assets/Project/Scripts/Player.cs
@@ -7,6 +7,7 @@ public class Player : MonoBehaviour
     public Vector3 Middle;
     public Vector3 Left;
     public Vector3 Right;
+    public Vector3 CurrentPosition;
     // Start is called before the first frame update
     void Start()
     {
@@ -16,6 +17,15 @@ public class Player : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        if (transform.position == Middle)
+            CurrentPosition = Middle;
+        if (transform.position == Left)
+            CurrentPosition = Left;
+        if (transform.position == Right)
+            CurrentPosition = Right;
+      /*  if (Input.GetKeyDown("a") || Input.GetKeyDown("LeftArrow"))
+        {
+            transform.position = Vector3.Lerp()
+        }*/
     }
 }
