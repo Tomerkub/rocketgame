829bd9b98a22deda7933819eff82d8b231154624
tomer <tomerkubovsky@gmail.com>
2020-10-20 20:09:28 +1100

    Doing The Bar
    Bar

--- Assets/Project/Scripts/Player.cs
+++ Assets/Project/Scripts/Player.cs
@@ -6,6 +6,7 @@ public class Player : MonoBehaviour
     public float speed = 3f;
     public float SpaceJunk;
     private Rigidbody2D SelfRigid;
+    [Range(0f,100f)] public float SpaceJunkCollected = 0f;
     // Start is called before the first frame update
     void Start()
     {
